9e7bb0a681d0239c66f21c5d73afd617bcb45ea6
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-03-08 14:45:35 +0530

    installed infinite-scroller

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <h2>Where in the world?</h2>
+        <div>
+          <h4>Dark Mode</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+// import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+function Homepage() {
+  return (
+    <div>
+      <div>
+        <span>
+          <SearchIcon />
+          <input placeholder="Search for a country..." />
+        </span>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default Homepage;
