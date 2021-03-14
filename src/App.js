fb54e9ca25e3d6a373c6102e2e1bb4a5e3c13218
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-03-14 19:58:56 +0530

    feat: wrap components in switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { Route, Switch } from "react-router-dom";
 import { Country } from "./contexts/state.context";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
+import CountryDetails from "./components/CountryDetails";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Country>
-        <Header />
-        <Homepage />
+        <Switch>
+          <Route path="/" exact render={(props) => <Homepage {...props} />} />
+          <Route
+            path="/detail/:id"
+            exact
+            render={(routeProps) => <CountryDetails {...routeProps} />}
+          />
+        </Switch>
       </Country>
     </div>
   );
