8f2b8e98b1e20969508c6cf6bab9c0baeca12b5e
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-03-15 17:47:48 +0530

    add component displaying country details & redirects to its border countries

---
+++ src/components/CountryDetails.js
@@ -0,0 +1,86 @@
+import React, { useContext } from "react";
+import { allCountriesData } from "../contexts/state.context";
+
+function CountryDetails({ match, history }) {
+  const allCountries = useContext(allCountriesData);
+
+  const countryFound = allCountries.find(
+    (s) => s.name.toLowerCase() === match.params.id.toLowerCase()
+  );
+
+  const displayNameFromCode = (borderCode) => {
+    const borderCountry = allCountries.find(
+      (coun) => coun.alpha3Code === borderCode
+    );
+    return (
+      <button onClick={() => history.push(`/detail/${borderCountry.name}`)}>
+        {borderCountry.name}
+      </button>
+    );
+  };
+
+  const print = countryFound ? (
+    <div>
+      <img src={countryFound.flag} alt={`flag of ${countryFound.name}`} />
+      <div>
+        <h2>{countryFound.name}</h2>
+        <div>
+          <p>
+            <strong>Native Name: </strong>
+            {countryFound.nativeName}
+          </p>
+          <p>
+            <strong>Population: </strong>
+            {countryFound.population.toLocaleString()}
+          </p>
+          <p>
+            <strong>Region: </strong>
+            {countryFound.region}
+          </p>
+          <p>
+            <strong>Sub Region: </strong>
+            {countryFound.subregion}
+          </p>
+          <p>
+            <strong>Capital: </strong>
+            {countryFound.capital}
+          </p>
+        </div>
+      </div>
+      <div>
+        <p>
+          <strong>Top Level Domain: </strong>
+          {countryFound.topLevelDomain.join(", ")}
+        </p>
+        <p>
+          <strong>Currencies: </strong>
+          {countryFound.currencies.map((c) => c.name).join(", ")}
+        </p>
+        <p>
+          <strong>Language: </strong>
+          {countryFound.languages.map((l) => l.name).join(", ")}
+        </p>
+      </div>
+      {countryFound?.borders.length !== 0 && (
+        <div>
+          <strong>Border Countries:</strong>
+          {countryFound.borders.map((b) => displayNameFromCode(b))}
+        </div>
+      )}
+    </div>
+  ) : (
+    <div>Country doesn't exist.</div>
+  );
+
+  // return countryFound ? showDetails : notFound;
+  return (
+    <>
+      <div>
+        <button onClick={() => history.push("/")}>Back</button>
+      </div>
+      {print}
+    </>
+  );
+}
+
+export default CountryDetails;
