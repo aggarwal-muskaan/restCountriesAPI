62bc04904f8cd1173cbe374073b7fc762cd60e43
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-05-02 20:22:37 +0530

    add dark mode with styled-components
    1) add custom hook
    2) add global styles file
    3) created variables of colors
    4) pass theme prop that changes background defined in Theme.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-// import logo from './logo.svg';
-// import './App.css';
 import { Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styles/Theme";
+import { GlobalStyles } from "./styles/globalStyles";
+
+import { useDarkMode } from "./customhook/useDarkMode";
 import { Country } from "./contexts/state.context";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 import CountryDetails from "./components/CountryDetails";
 
 function App() {
+  const [theme, toggleTheme] = useDarkMode("light");
+
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+
+  // if (!mountedComponent) return <div />;
+
   return (
     <div className="App">
-      <Header />
-      <Country>
-        <Switch>
-          <Route path="/" exact render={(props) => <Homepage {...props} />} />
-          <Route
-            path="/detail/:id"
-            exact
-            render={(routeProps) => <CountryDetails {...routeProps} />}
-          />
-        </Switch>
-      </Country>
+      <ThemeProvider theme={themeMode}>
+        <>
+          <GlobalStyles />
+          <Header theme={theme} toggleTheme={toggleTheme} />
+          <Country>
+            <Switch>
+              <Route
+                path="/"
+                exact
+                render={(props) => <Homepage {...props} />}
+              />
+              <Route
+                path="/detail/:id"
+                exact
+                render={(routeProps) => <CountryDetails {...routeProps} />}
+              />
+            </Switch>
+          </Country>
+        </>
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Brightness2OutlinedIcon from "@material-ui/icons/Brightness2Outlined";
+import Brightness2Icon from "@material-ui/icons/Brightness2";
 
-function Header() {
+function Header({ theme, toggleTheme }) {
   return (
     <div>
       <div>
         <h2>Where in the world?</h2>
         <div>
-          <Brightness2OutlinedIcon />
+          {theme === "dark" ? (
+            <Brightness2Icon onClick={toggleTheme} />
+          ) : (
+            <Brightness2OutlinedIcon onClick={toggleTheme} />
+          )}
           <h4>Dark Mode</h4>
         </div>
       </div>
